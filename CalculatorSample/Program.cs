401d057e561f25ffb6d9d82d86a3dd59d10bb97b
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2023-07-20 23:37:14 -0400

    add minimum viable product

--- CalculatorSample/Program.cs
+++ CalculatorSample/Program.cs
@@ -1,5 +1,41 @@
 ﻿Console.WriteLine("Welcome to the calculator!");
 
+int accumulator = 0;
+string input;
 
+Console.WriteLine("Initial value: " + accumulator);
+
+do
+{
+    // Get operation from user
+    Console.Write("Enter operation: ");
+    string operation = Console.ReadLine();
+
+    // Get operand from user
+    Console.Write("Enter operand: ");
+    input = Console.ReadLine();
+    int operand = int.Parse(input);
+
+    switch (operation)
+    {
+        case "+":
+            accumulator = accumulator + operand;
+            break;
+        case "-":
+            accumulator = accumulator - operand;
+            break;
+        case "x":
+            accumulator = accumulator * operand;
+            break;
+        case "/":
+            accumulator = accumulator / operand;
+            break;
+    }
+
+    Console.WriteLine("Current value: " + accumulator);
+
+    Console.Write("Press enter to continue or 'quit' to stop: ");
+    input = Console.ReadLine();
+} while (input != "quit");
 
 Console.WriteLine("Thank you for using the calculator!");
