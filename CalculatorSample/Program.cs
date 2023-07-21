7b659bcfbd4ec5b11dca410eee279498462e301c
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2023-07-20 23:59:03 -0400

    use string interpolation

--- CalculatorSample/Program.cs
+++ CalculatorSample/Program.cs
@@ -1,48 +1,54 @@
-﻿Console.WriteLine("Welcome to the calculator!");
-
-int accumulator = 0;
-
-Console.Write("Enter an initial value (press enter for 0): ");
-string input = Console.ReadLine();
-
-if (input != "")
+﻿class CalculatorSample
 {
-    accumulator = int.Parse(input);
-}
-
-Console.WriteLine("Initial value: " + accumulator);
-
-do
-{
-    Console.Write("Enter operation: ");
-    string operation = Console.ReadLine();
-
-    Console.Write("Enter operand (availabled operands: +, -, x, /): ");
-    input = Console.ReadLine();
-    int operand = int.Parse(input);
-
-    switch (operation)
+    public static void Main(string[] args)
     {
-        case "+":
-            accumulator += operand;
-            break;
-        case "-":
-            accumulator -= operand;
-            break;
-        case "x":
-        case "*":
-            accumulator *= operand;
-            break;
-        case "/":
-            accumulator /= operand;
-            break;
+        Console.WriteLine("Welcome to the calculator!");
+
+        int accumulator = 0;
+
+        Console.Write("Enter an initial value (press enter for 0): ");
+        string input = Console.ReadLine();
+
+        if (input != "")
+        {
+            accumulator = int.Parse(input);
+        }
+
+        Console.WriteLine($"Initial value: {accumulator}");
+
+        do
+        {
+            Console.Write("Enter operation: ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Enter operand (availabled operands: +, -, x, /): ");
+            input = Console.ReadLine();
+            int operand = int.Parse(input);
+
+            switch (operation)
+            {
+                case "+":
+                    accumulator += operand;
+                    break;
+                case "-":
+                    accumulator -= operand;
+                    break;
+                case "x":
+                case "*":
+                    accumulator *= operand;
+                    break;
+                case "/":
+                    accumulator /= operand;
+                    break;
+            }
+
+            Console.WriteLine($"Current value: {accumulator}");
+
+            Console.Write("Press enter to continue or 'quit' to stop: ");
+            input = Console.ReadLine();
+            input = input.ToLower();
+        } while (input is not "quit" and not "q");
+
+        Console.WriteLine("Thank you for using the calculator!");
     }
-
-    Console.WriteLine("Current value: " + accumulator);
-
-    Console.Write("Press enter to continue or 'quit' to stop: ");
-    input = Console.ReadLine();
-    input = input.ToLower();
-} while (input is not "quit" and not "q");
-
-Console.WriteLine("Thank you for using the calculator!");
+}
