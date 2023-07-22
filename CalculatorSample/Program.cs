6b50fb51bcfd7e797f160a209c2338f0a6a4bc91
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2023-07-21 22:44:39 -0400

    pull out logic into methods

--- CalculatorSample/Program.cs
+++ CalculatorSample/Program.cs
@@ -1,9 +1,27 @@
 ﻿class CalculatorSample
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         Console.WriteLine("Welcome to the calculator!");
 
+        int accumulator = InitializeAccumulator();
+
+        Console.WriteLine($"Initial value: {accumulator}");
+
+        do
+        {
+            string operation = GetOperationFromUser();
+            int operand = GetOperandFromUser();
+            accumulator = CalculateNewValue(accumulator, operation, operand);
+
+            Console.WriteLine($"Current value: {accumulator}");
+        } while (ShouldContinue());
+
+        Console.WriteLine("Thank you for using the calculator!");
+    }
+
+    private static int InitializeAccumulator()
+    {
         int accumulator = 0;
 
         Console.Write("Enter an initial value (press enter for 0): ");
@@ -14,26 +32,25 @@
             accumulator = int.Parse(input);
         }
 
-        Console.WriteLine($"Initial value: {accumulator}");
-
-        do
-        {
-            Console.Write("Enter operation (available operations: +, -, x, /): ");
-            string operation = Console.ReadLine() ?? "";
-
-            Console.Write("Enter operand: ");
-            input = Console.ReadLine() ?? "";
-            int operand = int.Parse(input);
-
-            accumulator = CalculateNewValue(accumulator, operation, operand);
-
-            Console.WriteLine($"Current value: {accumulator}");
-        } while (ShouldContinue());
-
-        Console.WriteLine("Thank you for using the calculator!");
+        return accumulator;
     }
 
-    static int CalculateNewValue(int currentValue, string? operation, int operand)
+    private static int GetOperandFromUser()
+    {
+        Console.Write("Enter operand: ");
+        string input = Console.ReadLine() ?? "";
+        int operand = int.Parse(input);
+        return operand;
+    }
+
+    private static string GetOperationFromUser()
+    {
+        Console.Write("Enter operation (available operations: +, -, x, /): ");
+        string operation = Console.ReadLine() ?? "";
+        return operation;
+    }
+
+    private static int CalculateNewValue(int currentValue, string? operation, int operand)
     {
         int newValue = operation switch
         {
@@ -49,9 +66,9 @@
     static bool ShouldContinue()
     {
         Console.Write("Press enter to continue or 'quit' to stop: ");
-        string? userInput = Console.ReadLine();
-        userInput = userInput?.ToLower();
-        bool shouldContinue = userInput is not "quit" and not "q";
+        string? input = Console.ReadLine();
+        input = input?.ToLower();
+        bool shouldContinue = input is not "quit" and not "q";
         return shouldContinue;
     }
 }
